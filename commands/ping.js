1953173a7d411ed06af8a4d9dfc814f129579559
Isaiahjohnson6901 <60998555+Isaiahjohnson6901@users.noreply.github.com>
2021-12-02 17:42:16 -0500

    IDK

--- commands/ping.js
+++ commands/ping.js
@@ -4,17 +4,21 @@ const { MessageEmbed } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('Replies with Pong!'),
+		.setDescription('Replies with Pong!')
+		.addUserOption(option => option.setName('target').setDescription('Select a user')),
+
 	async execute(interaction) {
 	const top = "<:Top_icon:916087719397036113>";
 	const jungle = "<:jungleicon:916087719430590474> ";
 	const mid = "<:midicon:916087719749369856>";
 	const ADC = "<:adcicon:916087719095074827>";
 	const support = "<:supporticon:916087719413825637>";
+	const user = interaction.options.getUser('target');
+	console.log(user);
 
 	const exampleEmbed = new MessageEmbed()
 		.setColor('#660066')
-		.setTitle('Lobby created for 8:30pm EST')
+		.setTitle(`Lobby created for ${user}`)
 		.addField(top, '\u200B')
 		.addField(jungle, '\u200B')
 		.addField(mid, '\u200B')
