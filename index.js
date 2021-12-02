47f242b79c969a1f4276e4078c7e341dc7551c4d
isaiahjohnson6901 <Isaiahjohnson6901@gmail.com>
2021-12-02 02:47:13 -0500

    Made a command handler

--- index.js
+++ index.js
@@ -12,21 +12,30 @@ client.once('ready', () => {
 });
 
 client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported modules
+	client.commands.set(command.data.name, command);
+}
 
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
-	const { commandName } = interaction;
+	const command = client.commands.get(interaction.commandName);
 
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	} else if (commandName === 'beep') {
-		await interaction.reply('Boop!');
-	} else if (commandName === 'server') {
-		await interaction.reply('This server sucks');
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
-	
 });
+
 
 // Login to Discord with your client's token
 client.login(token);
