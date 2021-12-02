ef0b2991c5dfc44ea09ee906ca9835077f30a25f
Isaiahjohnson6901 <60998555+Isaiahjohnson6901@users.noreply.github.com>
2021-12-02 17:19:53 -0500

    Starting the startqueue.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,26 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder, channelMention } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+	const top = "<:Top_icon:916087719397036113>";
+	const jungle = "<:jungleicon:916087719430590474> ";
+	const mid = "<:midicon:916087719749369856>";
+	const ADC = "<:adcicon:916087719095074827>";
+	const support = "<:supporticon:916087719413825637>";
+
+	const exampleEmbed = new MessageEmbed()
+		.setColor('#660066')
+		.setTitle('Lobby created for 8:30pm EST')
+		.addField(top, '\u200B')
+		.addField(jungle, '\u200B')
+		.addField(mid, '\u200B')
+		.addField(ADC, '\u200B')
+		.addField(support, '\u200B')
+		.setFooter('Use !queue [role] to join or !leave to leave. | All non-queue messages will be deleted.');
+		await interaction.reply({ embeds: [exampleEmbed] });
 	},
 };
